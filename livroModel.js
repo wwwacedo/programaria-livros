aa4c409f0d6141dbdddbb4c1ea508128273465d1
wwwacedo <eng.marianammacedo@gmail.com>
2023-06-21 09:57:33 -0300

    Alterando propriedade 'imagemCapa' para 'imagem' no livroModel

--- livroModel.js
+++ livroModel.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const livroSchema = mongoose.Schema({
 	autora: {type: String, required: true},
 	nome: {type: String, required: true},
-	imagemCapa: {type: String, required: true},
+	imagem: {type: String, required: true},
 	categoria: {type: String, required: true},
 });
 
